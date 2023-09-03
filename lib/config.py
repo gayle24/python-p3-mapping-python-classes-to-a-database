7c200a9ad51df17da3060e2625d0d59f1c198c86
Abigail Amani Wanjiku <abigail.wanjiku@student.moringaschool.com>
2023-09-03 11:18:31 +0300

    song.py

--- lib/config.py
+++ lib/config.py
@@ -2,3 +2,10 @@ import sqlite3
 
 CONN = sqlite3.connect('music.db')
 CURSOR = CONN.cursor()
+# Assuming you've already connected to the database and created a cursor
+CURSOR.execute("PRAGMA table_info(songs)")
+table_info = CURSOR.fetchall()
+print(table_info)
+
+songs = CURSOR.execute('SELECT * FROM songs')
+[row for row in songs]
--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,36 @@
 from config import CONN, CURSOR
 
 class Song:
-    pass
+    def __init__(self, name, album):
+        self.name = name
+        self.album = album
+
+    @classmethod
+    def create_table(self):
+        sql = """
+            CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+        """
+
+        CURSOR.execute(sql)
+
+    def save(self):
+        sql = '''
+            INSERT INTO songs(name, album)
+            VALUES (?, ?)
+        '''
+
+        CURSOR.execute(sql, (self.name, self.album))
+        
+Song.create_table()
+
+hello = Song("Hello", "25")
+hello.save()
+
+despacito = Song("Despacito", "Vida")
+despacito.save()
+
+Song.save(hello)
